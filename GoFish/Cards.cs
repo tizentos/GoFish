7ea4c08b0600460d9accca1c81b136abb4a6449f
tizentos <tizentos@gmail.com>
2017-12-02 15:39:36 +0100

    Add project files.

---
+++ GoFish/Cards.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish
+{
+    partial class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public string Name { get { return Value.ToString() + " of " + Suit.ToString(); } }
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public static string Plural(Values value)
+        {
+            if (value == Values.Six)
+                return "Sixes";
+            else
+                return value.ToString() + "s";
+        }
+    }
+         enum Suits
+        {
+            Spades,
+            Clubs,
+            Diamonds,
+            Hearts
+        }
+         enum Values
+        {
+            Ace = 1,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }
+        class CardComparer_bySuit : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                if (x.Suit > y.Suit)
+                    return 1;
+                if (x.Suit < y.Suit)
+                    return -1;
+                if (x.Value > y.Value)
+                    return 1;
+                if (x.Value < y.Value)
+                    return -1;
+                return 0;
+            }
+        }
+        class CardComparer_byValue : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                if (x.Value > y.Value)
+                    return 1;
+                if (x.Value < y.Value)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+}
---
+++ GoFish/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace GoFish
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listHand = new System.Windows.Forms.ListBox();
+            this.buttonAsk = new System.Windows.Forms.Button();
+            this.textProgress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBooks = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Books";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Your hand";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(12, 25);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(123, 20);
+            this.textName.TabIndex = 3;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(141, 24);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(124, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start the game!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listHand
+            // 
+            this.listHand.FormattingEnabled = true;
+            this.listHand.Location = new System.Drawing.Point(271, 26);
+            this.listHand.Name = "listHand";
+            this.listHand.Size = new System.Drawing.Size(143, 303);
+            this.listHand.TabIndex = 5;
+            // 
+            // buttonAsk
+            // 
+            this.buttonAsk.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.buttonAsk.Enabled = false;
+            this.buttonAsk.Location = new System.Drawing.Point(271, 342);
+            this.buttonAsk.Name = "buttonAsk";
+            this.buttonAsk.Size = new System.Drawing.Size(144, 23);
+            this.buttonAsk.TabIndex = 6;
+            this.buttonAsk.Text = "Ask for a card";
+            this.buttonAsk.UseVisualStyleBackColor = true;
+            this.buttonAsk.Click += new System.EventHandler(this.buttonAsk_Click);
+            // 
+            // textProgress
+            // 
+            this.textProgress.Location = new System.Drawing.Point(12, 66);
+            this.textProgress.Multiline = true;
+            this.textProgress.Name = "textProgress";
+            this.textProgress.ReadOnly = true;
+            this.textProgress.Size = new System.Drawing.Size(252, 219);
+            this.textProgress.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Game Progress";
+            // 
+            // textBooks
+            // 
+            this.textBooks.Location = new System.Drawing.Point(12, 305);
+            this.textBooks.Multiline = true;
+            this.textBooks.Name = "textBooks";
+            this.textBooks.ReadOnly = true;
+            this.textBooks.Size = new System.Drawing.Size(252, 60);
+            this.textBooks.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 377);
+            this.Controls.Add(this.textBooks);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textProgress);
+            this.Controls.Add(this.buttonAsk);
+            this.Controls.Add(this.listHand);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listHand;
+        private System.Windows.Forms.Button buttonAsk;
+        private System.Windows.Forms.TextBox textProgress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBooks;
+    }
+}
+
